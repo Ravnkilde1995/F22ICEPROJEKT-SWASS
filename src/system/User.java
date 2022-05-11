8703cdbc299a4aa1a1573d52f5b92b0009a2dd32
Oliver <Ravnkilde1995>
2022-05-11 16:28:18 +0200

    Initial commit

---
+++ src/system/User.java
@@ -0,0 +1,111 @@
+package system;
+
+public class User {
+
+    //Fields
+    //***********************
+
+    int userID;
+    String name;
+    String lastName;
+    int socialSecurityNumber;
+    String gender;
+    String username;
+    String password;
+    int phoneNumber;
+    int balance;
+
+    //Constructor
+    //***********************
+
+
+
+    //Methods
+    //***********************
+    public int getUserId() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(int socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", socialSecurityNumber=" + socialSecurityNumber +
+                ", gender='" + gender + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", balance=" + balance +
+                '}';
+    }
+}
